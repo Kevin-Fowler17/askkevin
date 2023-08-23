646edd590609eb80fb34616fb4c7ac925654cc76
Kevin Fowler <kevin.m.fowler17@gmail.com>
2023-08-23 14:51:53 -0500

    initial commit

---
+++ src/test/java/net/askkevin/askkevin/AskkevinApplicationTests.java
@@ -0,0 +1,13 @@
+package net.askkevin.askkevin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AskkevinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
