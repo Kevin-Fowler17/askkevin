150a83f290cc3ab0d8a95e5d4ed994c930e9e917
Kevin Fowler <kevin.m.fowler17@gmail.com>
2023-09-11 15:32:53 -0500

    medical history pages are accessible

---
+++ src/main/java/net/askkevin/askkevin/controllers/SurgeriesController.java
@@ -0,0 +1,34 @@
+package net.askkevin.askkevin.controllers;
+
+import lombok.AllArgsConstructor;
+import net.askkevin.askkevin.models.User;
+import net.askkevin.askkevin.repositories.UserRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@AllArgsConstructor
+@Controller
+public class SurgeriesController {
+
+    private final UserRepository userDao;
+
+    @GetMapping("/user/{id}/surgeries")
+    @Transactional
+    public String showSurgeriesForm(@PathVariable long id, Model model) {
+
+        User loggedInUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        if (loggedInUser == null) {
+            return "/user";
+        }
+
+        User user = userDao.getReferenceById(loggedInUser.getId());
+
+        return "users/surgeries";
+    }
+
+}
