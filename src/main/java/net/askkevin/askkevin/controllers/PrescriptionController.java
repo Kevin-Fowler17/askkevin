3104cfb833b379157fd1d5e0479286950ad1ec49
Kevin Fowler <kevin.m.fowler17@gmail.com>
2023-10-04 15:43:54 -0500

    working on controller for prescriptions

--- src/main/java/net/askkevin/askkevin/controllers/PrescriptionController.java
+++ src/main/java/net/askkevin/askkevin/controllers/PrescriptionController.java
@@ -1,14 +1,21 @@
 package net.askkevin.askkevin.controllers;
 
 import lombok.AllArgsConstructor;
+import net.askkevin.askkevin.models.Insurance;
+import net.askkevin.askkevin.models.Prescription;
 import net.askkevin.askkevin.models.User;
+import net.askkevin.askkevin.repositories.PrescriptionRepository;
 import net.askkevin.askkevin.repositories.UserRepository;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.lang.model.element.NestingKind;
 
 @AllArgsConstructor
 @Controller
@@ -16,8 +23,9 @@ public class PrescriptionController {
 
     private final UserRepository userDao;
 
+    private final PrescriptionRepository prescriptionDao;
+
     @GetMapping("/user/{id}/prescriptions")
-    @Transactional
     public String showPrescriptionsForm(@PathVariable long id, Model model) {
 
         User loggedInUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
@@ -28,7 +36,24 @@ public class PrescriptionController {
 
         User user = userDao.getReferenceById(loggedInUser.getId());
 
+        model.addAttribute("prescription", new Prescription());
+
         return "users/prescriptions";
+    }
+
+    @PostMapping("/user/{id}/prescriptions")
+    public String submitPrescriptionsForm(@ModelAttribute Prescription prescription, Model model, @PathVariable long id) {
+
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        model.addAttribute("user", user);
+
+        prescription.setUser(user);
+
+        prescriptionDao.save(prescription);
+
+        return "users/prescriptions";
+
     }
 
 }
--- src/main/java/net/askkevin/askkevin/models/Prescription.java
+++ src/main/java/net/askkevin/askkevin/models/Prescription.java
@@ -18,66 +18,12 @@ public class Prescription {
     private long id;
 
     @Column(nullable = true)
-    private String p1a;
+    private String name;
 
     @Column(nullable = true)
-    private String p1b;
+    private String dosage;
 
-    @Column(nullable = true)
-    private String p2a;
-
-    @Column(nullable = true)
-    private String p2b;
-
-    @Column(nullable = true)
-    private String p3a;
-
-    @Column(nullable = true)
-    private String p3b;
-
-    @Column(nullable = true)
-    private String p4a;
-
-    @Column(nullable = true)
-    private String p4b;
-
-    @Column(nullable = true)
-    private String p5a;
-
-    @Column(nullable = true)
-    private String p5b;
-
-    @Column(nullable = true)
-    private String p6a;
-
-    @Column(nullable = true)
-    private String p6b;
-
-    @Column(nullable = true)
-    private String p7a;
-
-    @Column(nullable = true)
-    private String p7b;
-
-    @Column(nullable = true)
-    private String p8a;
-
-    @Column(nullable = true)
-    private String p8b;
-
-    @Column(nullable = true)
-    private String p9a;
-
-    @Column(nullable = true)
-    private String p9b;
-
-    @Column(nullable = true)
-    private String p10a;
-
-    @Column(nullable = true)
-    private String p10b;
-
-    @OneToOne
+    @ManyToOne
     @JoinColumn (nullable = false, name = "user_id")
     private User user;
 
