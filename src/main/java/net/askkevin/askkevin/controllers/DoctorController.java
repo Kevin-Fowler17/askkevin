dbbee0eaac9b3ff1e16f0e359ff583c2e51e4187
Kevin Fowler <kevin.m.fowler17@gmail.com>
2023-10-06 13:22:58 -0500

    trying to figure out why insurance page isn't working

--- src/main/java/net/askkevin/askkevin/controllers/DoctorController.java
+++ src/main/java/net/askkevin/askkevin/controllers/DoctorController.java
@@ -1,20 +1,26 @@
 package net.askkevin.askkevin.controllers;
 
 import lombok.AllArgsConstructor;
+import net.askkevin.askkevin.models.Doctor;
 import net.askkevin.askkevin.models.User;
+import net.askkevin.askkevin.repositories.DoctorRepository;
 import net.askkevin.askkevin.repositories.UserRepository;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @AllArgsConstructor
 @Controller
 public class DoctorController {
 
     private final UserRepository userDao;
+
+    private final DoctorRepository doctorDao;
 
     @GetMapping("/user/{id}/doctors")
     @Transactional
@@ -29,6 +35,21 @@ public class DoctorController {
         User user = userDao.getReferenceById(loggedInUser.getId());
 
         return "users/doctors";
+    }
+
+    @PostMapping("/user/{id}/doctors")
+    public String submitDoctorsForm(@ModelAttribute Doctor doctor, Model model, @PathVariable long id) {
+
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        model.addAttribute("user", user);
+
+        doctor.setUser(user);
+
+        doctorDao.save(doctor);
+
+        return "users/doctors";
+
     }
 
 }
