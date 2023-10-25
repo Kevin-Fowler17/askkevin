6329897523a9f2d50a9eb2131ab6c5999991aefe
Kevin Fowler <kevin.m.fowler17@gmail.com>
2023-10-25 14:45:11 -0500

    working on surgery controller

--- src/main/java/net/askkevin/askkevin/controllers/SurgeryController.java
+++ src/main/java/net/askkevin/askkevin/controllers/SurgeryController.java
@@ -1,7 +1,11 @@
 package net.askkevin.askkevin.controllers;
 
 import lombok.AllArgsConstructor;
+import net.askkevin.askkevin.models.Prescription;
+import net.askkevin.askkevin.models.Surgery;
 import net.askkevin.askkevin.models.User;
+import net.askkevin.askkevin.repositories.PrescriptionRepository;
+import net.askkevin.askkevin.repositories.SurgeryRepository;
 import net.askkevin.askkevin.repositories.UserRepository;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
@@ -16,6 +20,8 @@ public class SurgeryController {
 
     private final UserRepository userDao;
 
+    private final SurgeryRepository surgeryDao;
+
     @GetMapping("/user/{id}/surgeries")
     @Transactional
     public String showSurgeriesForm(@PathVariable long id, Model model) {
@@ -27,6 +33,8 @@ public class SurgeryController {
         }
 
         User user = userDao.getReferenceById(loggedInUser.getId());
+
+        model.addAttribute("surgery", new Surgery());
 
         return "users/surgeries";
     }
